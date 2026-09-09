8600f92b99347ce0a0ace3f1a303c804938cb394
Kanox1221 <Kanox1221x@gmail.com>
2026-09-09 18:24:12 +0900

    [Feature] Server Message Send to Json

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 const PORT = 8080;
 
+app.use(cors());
+
 app.get('/api/home', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.json({message: "Hello World!"})
 });
 
 app.listen(PORT, () => {
