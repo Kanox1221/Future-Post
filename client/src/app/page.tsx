cb259a6cafe2a4aad004d2b7421ae33a8d0f0806
Kanox1221 <Kanox1221x@gmail.com>
2026-09-09 19:15:38 +0900

    [Feature] Basic Design of Intro Page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,22 +3,14 @@
 import React, {useEffect, useState} from 'react';
 
 function Home(){
-
-	const [message, setMessage] = useState("Loading");
-
-  useEffect(() => {
-	fetch("http://localhost:8080/api/home").then(
-	  response => response.json()
-	).then(
-	  data => {
-		console.log(data)
-		setMessage(data.message)
-	  }
-	)
-  }, [])
-
   return(
-	<div>{message}</div>
+		<div id="intro-page" className='bg-cream'>
+			<div className='intro-container'>
+				<h1 className='intro-title text-4xl text-text-dark mb-6'>Future Post</h1>
+				<p className='intro-desc text-text-mid mb-4'>Send a post to future yourself</p>
+				<button className='btn-start bg-brown text-white shadow-xl rounded-full px-4 py-2'>Send Post</button>
+			</div>
+		</div>
   )
 }
 
